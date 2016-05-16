d5231b1b2a45d5d454db847a3ba93c48b3b91077
SandroGolis <golis.sandro@gmail.com>
2016-05-16 20:34:05 +0300

    Actions list bounded to selected session + icons

--- src/ThreadsToolMainWindow.java
+++ src/ThreadsToolMainWindow.java
@@ -1,21 +1,19 @@
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
+import java.util.List;
 
-/**
- * Created by Sandro on 5/5/2016.
- */
 public class ThreadsToolMainWindow extends JFrame
 {
     private final ArrayList<UserSession> sessions;
     private JPanel MainPanel;
     private JList<UserSession> SessionsList;
+    private JList<UserAction> ActionsList;
     private DefaultListModel<UserSession> SessionsModel;
+    private DefaultListModel<UserAction> ActionsModel;
 
-//    private JPanel LeftMenu;
-//    private JTextArea RightMenu;
-
-    public ThreadsToolMainWindow(String title, ArrayList<UserSession> userSessions)
+    ThreadsToolMainWindow(String title, ArrayList<UserSession> userSessions)
     {
         super(title);
         this.sessions = userSessions;
@@ -25,23 +23,14 @@ public class ThreadsToolMainWindow extends JFrame
         initSessionsModel();
         SessionsList.setModel(SessionsModel);
         SessionsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        SessionsList.setCellRenderer(new SessionRenderer());
+        SessionsList.setCellRenderer(new ListRenderer());
+        SessionsList.addListSelectionListener(this::sessionSelectionChanged);
 
 
-//        // set Layout manager
-//        MainPanel.setLayout(new BorderLayout());
-//
-//        initRightMenu();
-//        initLeftMenu(sessions);
-//
-//        JScrollBar scrollBar = new JScrollBar(JScrollBar.VERTICAL, 30, 20, 0, 300);
-//        scrollBar.setUnitIncrement(2);
-//        MainPanel.add(scrollBar, BorderLayout.CENTER);
-//
-//
-//        // Add component to content pane
-//        MainPanel.add(RightMenu, BorderLayout.CENTER);
-//        MainPanel.add(LeftMenu, BorderLayout.WEST);
+        ActionsModel = new DefaultListModel<>();
+        ActionsList.setModel(ActionsModel);
+        ActionsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        ActionsList.setCellRenderer(new ListRenderer());
 
 
         ImageIcon icon = new ImageIcon(getClass().getResource("/icons/VisualisationToolIcon.png"));
@@ -62,89 +51,49 @@ public class ThreadsToolMainWindow extends JFrame
         }
     }
 
-//    void initRightMenu()
-//    {
-//        RightMenu = new JTextArea();
-//        RightMenu.setBackground(Color.WHITE);
-//        RightMenu.setFont(RightMenu.getFont().deriveFont(16f));
-//        RightMenu.setForeground(Color.BLACK);
-//    }
-//
-//    void initLeftMenu(ArrayList<UserSession> sessions)
-//    {
-//        LeftMenu = new JPanel();
-//        Dimension d = LeftMenu.getPreferredSize();
-//        d.width = 400;
-//        LeftMenu.setPreferredSize(d);
-//        LeftMenu.setBorder(BorderFactory.createTitledBorder("Sessions"));
-//        LeftMenu.setBackground(Color.getColor("658017"));
-//
-//        LeftMenu.setLayout(new GridBagLayout());
-//        GridBagConstraints gc = new GridBagConstraints();
-//
-//        JButton buttons[];
-//        buttons = new JButton[sessions.size() + 1];
-//        gc.weightx = 0.5;
-//        gc.weighty = 0.5;
-//        gc.gridx = 0;
-//        gc.gridy = 0;
-//
-//        for ( int i = sessions.size();  i > 0; --i) {
-//            buttons[i] = new JButton("Session #" + i);
-//            buttons[i].setBackground(Color.WHITE);
-//            buttons[i].setForeground(Color.WHITE);
-//            buttons[i].setBorderPainted(true);
-//            Dimension d2 = buttons[i].getPreferredSize();
-//            d2.width = 50;
-//            buttons[i].setSize(d2);
-//            final int j = i;
-//            buttons[i].addActionListener(new ActionListener() {
-//                @Override
-//                public void actionPerformed(ActionEvent e) {
-//                    RightMenu.append("ShowThread Info Of Session #" + String.valueOf(j) + "\n");
-//                }
-//            });
-//            LeftMenu.add(buttons[i], gc);
-//            gc.gridy += 1;
-//        }
-//    }
-}
-
-class SessionRenderer extends JLabel implements ListCellRenderer {
-
-    public SessionRenderer() {
-        setOpaque(true);
-        setIconTextGap(12);
+    private void initActionsModel(List<UserAction> actions)
+    {
+        for (UserAction action :
+                actions)
+        {
+            ActionsModel.addElement(action);
+        }
     }
 
-    public Component getListCellRendererComponent(JList list, Object value,
-                                                  int index, boolean isSelected, boolean cellHasFocus) {
-        UserSession entry = (UserSession) value;
-        setText(entry.toString());
-        if (isSelected) {
-            setBackground(list.getSelectionBackground());
-            setForeground(list.getSelectionForeground());
-        } else {
-            setBackground(list.getBackground());
-            setForeground(list.getForeground());
+    private void sessionSelectionChanged(ListSelectionEvent e)
+    {
+        if (!e.getValueIsAdjusting())
+        {
+            ActionsModel.clear();
+            if (!SessionsList.isSelectionEmpty())
+            {
+                UserSession selectedSession = SessionsList.getSelectedValue();
+                initActionsModel(selectedSession.actions);
+            }
+        }
+    }
+}
+
+class ListRenderer extends DefaultListCellRenderer {
+    public Component getListCellRendererComponent(JList<?> list,
+                                                  Object value,
+                                                  int index,
+                                                  boolean isSelected,
+                                                  boolean cellHasFocus) {
+        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        setOpaque(true);
+        setIconTextGap(12);
+        if (value instanceof UserSession)
+        {
+            UserSession session = (UserSession) value;
+            setText((index + 1) + ". " + session.toString());
+        }
+        else if (value instanceof UserAction)
+        {
+            UserAction action = (UserAction) value;
+            setText((index + 1) + ". " + action.toString());
+            setIcon(action.getSmallIcon());
         }
         return this;
     }
 }
-
-//public class IngredientListCellRenderer extends DefaultListCellRenderer {
-//    public Component getListCellRendererComponent(JList<?> list,
-//                                                  Object value,
-//                                                  int index,
-//                                                  boolean isSelected,
-//                                                  boolean cellHasFocus) {
-//        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-//        if (value instanceof Ingredient) {
-//            Ingredient ingredient = (Ingredient)value;
-//            setText(ingredient.getName());
-//            setToolTipText(ingredient.getDescription());
-//            // setIcon(ingredient.getIcon());
-//        }
-//        return this;
-//    }
-//}
--- src/UserAction.java
+++ src/UserAction.java
@@ -1,8 +1,10 @@
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.nio.file.Path;
 
-/**
- * Created by Sandro on 5/7/2016.
- */
 class UserAction implements Comparable<UserAction>
 {
     String name;
@@ -15,11 +17,44 @@ class UserAction implements Comparable<UserAction>
     Object info;
 
     @Override
-    public int compareTo(UserAction otherAction) {
+    public int compareTo(@NotNull UserAction otherAction) {
         if (this.uaSeq < otherAction.uaSeq) {
             return -1;
         } else if (this.uaSeq > otherAction.uaSeq) {
             return 1;
         } else return 0;
     }
+
+    @Override
+    public String toString()
+    {
+        String title;
+        if (ctxName.isEmpty() && name.isEmpty())
+            title = "title unknown";
+        else
+            title = ctxName + ": " + name;
+
+        return title;
+    }
+
+    @SuppressWarnings("UndesirableClassUsage")
+    //TODO: consider storing small icons as members for faster rendering. (maybe the delay is from BufferedImage usage)
+    Icon getSmallIcon()
+    {
+        ImageIcon newIcon;
+        if (imagePath == null)
+        {
+            BufferedImage emptyImg = new BufferedImage(30, 40, BufferedImage.TYPE_INT_ARGB);
+            newIcon = new ImageIcon(emptyImg);
+        }
+        else
+        {
+            ImageIcon icon = new ImageIcon(this.imagePath.toString());
+            Image img = icon.getImage();
+            Image newImg = img.getScaledInstance(30, 40, java.awt.Image.SCALE_SMOOTH);
+            newIcon = new ImageIcon(newImg);
+        }
+        return newIcon;
+    }
+
 }
